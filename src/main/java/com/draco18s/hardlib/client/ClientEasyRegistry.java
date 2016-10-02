1fe8a391d86b346af067cda81eb0b03c4ec22653
Draco18s <draco18s@gmail.com>
2016-10-01 20:29:19 -0400

    ClientEasyRegistryFix
    Fix to self-invoking methods (refactor boo-boo)

--- src/main/java/com/draco18s/hardlib/client/ClientEasyRegistry.java
+++ src/main/java/com/draco18s/hardlib/client/ClientEasyRegistry.java
@@ -25,65 +25,65 @@ public class ClientEasyRegistry extends EasyRegistry {
 
 	@Override
 	public void _registerBlock(Block block, String registryname) {
-		this.registerBlock(block, registryname);
+		super.registerBlock(block, registryname);
 	}
 
 	@Override
 	public void _registerBlockWithItem(Block block, String registryname) {
-		this.registerBlockWithItem(block, registryname);
-		registerBlockResources(block);
+		super._registerBlockWithItem(block, registryname);
+		_registerBlockResources(block);
 	}
 
 	@Override
 	public void _registerBlockWithCustomItem(Block block, ItemBlock iBlock, String registryname) {
-		this.registerBlockWithCustomItem(block, iBlock, registryname);
+		super._registerBlockWithCustomItem(block, iBlock, registryname);
 		StateMapperBase b = new DefaultStateMapper();
 		BlockStateContainer bsc = block.getBlockState();
 		ImmutableList<IBlockState> values = bsc.getValidStates();
 		for(IBlockState state : values) {
-			registerBlockItemModelForMeta(block, block.getMetaFromState(state), b.getPropertyString(state.getProperties()));
+			_registerBlockItemModelForMeta(block, block.getMetaFromState(state), b.getPropertyString(state.getProperties()));
 		}
 	}
 
-	private void registerBlockResources(Block block) {
+	private void _registerBlockResources(Block block) {
 		Item item = Item.getItemFromBlock(block);
 		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(block.getRegistryName().toString()));
 	}
 
-	private void registerBlockItemModelForMeta(Block block, int metadata, String variant) {
+	private void _registerBlockItemModelForMeta(Block block, int metadata, String variant) {
 		final Item item = Item.getItemFromBlock(block);
 		if (item != null) {
-			registerItemModelForMeta(item, metadata, variant);
+			_registerItemModelForMeta(item, metadata, variant);
 		}
 	}
 
 	@Override
 	public void _registerItem(Item item, String registryname) {
-		this.registerItem(item, registryname);
-		registerItemModel(item);
+		super._registerItem(item, registryname);
+		_registerItemModel(item);
 	}
 
 	@Override
 	public <T extends IMetaLookup> void _registerItemWithVariants(Item item, String registryname, T variant) {
-		this.registerItemWithVariants(item, registryname, variant);
+		super._registerItemWithVariants(item, registryname, variant);
 		String variantName = variant.getID();
 		List<ItemStack> subItems = new ArrayList<ItemStack>();
 		item.getSubItems(item, CreativeTabs.SEARCH, subItems);
 		for (ItemStack stack : subItems) {
-			registerItemModelForMeta(item, stack.getMetadata(), variantName + "=" + variant.getByOrdinal(stack.getMetadata()));
+			_registerItemModelForMeta(item, stack.getMetadata(), variantName + "=" + variant.getByOrdinal(stack.getMetadata()));
 		}
 	}
 
-	private void registerItemModel(Item item) {
-		registerItemModel(item, item.getRegistryName().toString());
+	private void _registerItemModel(Item item) {
+		_registerItemModel(item, item.getRegistryName().toString());
 	}
 
-	private void registerItemModel(Item item, String modelLocation) {
+	private void _registerItemModel(Item item, String modelLocation) {
 		final ModelResourceLocation fullModelLocation = new ModelResourceLocation(modelLocation, "inventory");
-		registerItemModel(item, fullModelLocation);
+		_registerItemModel(item, fullModelLocation);
 	}
 
-	private void registerItemModel(Item item, final ModelResourceLocation fullModelLocation) {
+	private void _registerItemModel(Item item, final ModelResourceLocation fullModelLocation) {
 		ModelBakery.registerItemVariants(item, fullModelLocation); // Ensure the custom model is loaded and prevent the default model from being loaded
 		//registerItemModel(item, MeshDefinitionFix.create(stack -> fullModelLocation));
 		ModelLoader.setCustomMeshDefinition(item, new ItemMeshDefinition()
@@ -95,15 +95,15 @@ public class ClientEasyRegistry extends EasyRegistry {
 	    });
 	}/**/
 
-	private void registerItemModel(Item item, ItemMeshDefinition meshDefinition) {
+	private void _registerItemModel(Item item, ItemMeshDefinition meshDefinition) {
 		ModelLoader.setCustomMeshDefinition(item, meshDefinition);
 	}
 
-	private void registerItemModelForMeta(Item item, int metadata, String variant) {
-		registerItemModelForMeta(item, metadata, new ModelResourceLocation(item.getRegistryName(), variant));
+	private void _registerItemModelForMeta(Item item, int metadata, String variant) {
+		_registerItemModelForMeta(item, metadata, new ModelResourceLocation(item.getRegistryName(), variant));
 	}
 
-	private void registerItemModelForMeta(Item item, int metadata, ModelResourceLocation modelResourceLocation) {
+	private void _registerItemModelForMeta(Item item, int metadata, ModelResourceLocation modelResourceLocation) {
 		ModelLoader.setCustomModelResourceLocation(item, metadata, modelResourceLocation);
 	}
 
